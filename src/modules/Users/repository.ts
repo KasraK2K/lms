6a8e2b6a2c4e69cfc1e0429de53c73772c601e38
Kasra Karami <Kasra_K2K@yahoo.com>
2024-04-13 21:13:23 +0330

    add logger to repository

--- src/modules/Users/repository.ts
+++ src/modules/Users/repository.ts
@@ -3,6 +3,7 @@ import { Prisma } from '@prisma/client'
 // Modules
 import Repository from '#base/Repository'
 import db from '#db'
+import logger from '#logger'
 import { IPagination } from '#types/interfaces'
 import User from './entity'
 import { IUser, IUserFillable, IUserGuarded } from './interface'
@@ -36,26 +37,28 @@ class UserRepository extends Repository {
 	public async create(args: IUserFillable): Promise<IUser> {
 		try {
 			return await db.user.create({ data: args })
-		} catch (err) {
-			if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
-				const error = new Error('Record already exist')
-				error.name = 'Database'
-				throw error
+		} catch (error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+				const err = new Error('Record already exist')
+				err.name = 'Database'
+				throw err
 			}
-			throw err
+			logger('repository', { module: 'user', method: 'create', error })
+			throw error
 		}
 	}
 
 	public async update(values: IUserGuarded, id: number): Promise<IUser> {
 		try {
 			return await db.user.update({ where: { id }, data: values, select: new User().exclude(['password']) })
-		} catch (err) {
-			if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
-				const error = new Error('Record not found for update')
-				error.name = 'Database'
-				throw error
+		} catch (error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+				const err = new Error('Record not found for update')
+				err.name = 'Database'
+				throw err
 			}
-			throw err
+			logger('repository', { module: 'user', method: 'update', error })
+			throw error
 		}
 	}
 
@@ -70,13 +73,14 @@ class UserRepository extends Repository {
 				data: { archived_at: new Date() },
 				select: new User().exclude(['password']),
 			})
-		} catch (err) {
-			if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
-				const error = new Error('Record not found for delete')
-				error.name = 'Database'
-				throw error
+		} catch (error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+				const err = new Error('Record not found for delete')
+				err.name = 'Database'
+				throw err
 			}
-			throw err
+			logger('repository', { module: 'user', method: 'destroy', error })
+			throw error
 		}
 	}
 }
